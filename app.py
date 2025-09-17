848268f76442929366b238b2cf7dc84d59ba4917
Crabmann2025 <1990.c@gmx.de>
2025-09-17 09:25:38 +0200

    Created post.json, Updated app.py, static and templates added

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+# Funktion, um Blog-Beiträge aus der JSON-Datei zu laden
+def load_posts():
+    with open("posts.json", "r") as file:
+        return json.load(file)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def index():
+    blog_posts = load_posts()  # Beiträge laden
+    return render_template('index.html', posts=blog_posts)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
